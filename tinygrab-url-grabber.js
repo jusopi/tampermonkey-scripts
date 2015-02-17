fd6f287772f33d73f85f81c0c7de0ef9be145f23
jusopi <jwopitz@gmail.com>
2015-02-17 11:15:48 -0600

    write tinygrab img src to clipboard

--- tinygrab-url-grabber.js
+++ tinygrab-url-grabber.js
@@ -5,9 +5,19 @@
 // @description  Opens a TinyGrab image source url minus the wrapping page content that is the default behavior.
 // @author       jusopi
 // @match        http://grab.by/*
-// @grant        none
+// @grant        GM_setClipboard
 // @updateURL	 https://github.com/jusopi/tampermonkey-scripts.git
 // ==/UserScript==
 
-var elem = document.getElementById('thegrab')
-if (elem && elem.src) window.location.href = elem.src; //window.open(url);
+var elem = document.getElementById('thegrab');
+if (elem && elem.src)
+{
+    //copy to clipboard
+    GM_setClipboard(elem.src);
+    
+    //replace crnt tab
+    //window.location.href = elem.src; 
+    
+    //open new tab
+    //window.open(elem.src);
+}
